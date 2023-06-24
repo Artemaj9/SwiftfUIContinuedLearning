7011a61b5469ee6a702e22f8e2ec81b5c246ce93
Artem <moiseev@physics.msu.ru>
2023-06-24 15:20:57 +0300

    DragGesture screens added

---
+++ SwiftfulThinkingContinuedLearning/DragGestureBootcamp.swift
@@ -0,0 +1,66 @@
+//
+//  DragGestureBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Artem on 24.06.2023.
+//
+
+import SwiftUI
+
+struct DragGestureBootcamp: View {
+    
+    @State var offset: CGSize = .zero
+    
+    var body: some View {
+        ZStack {
+            
+            VStack {
+                Text("\(offset.width)")
+                Spacer()
+            }
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 500)
+                .offset(offset)
+                .scaleEffect(getScaleAmount())
+                .rotationEffect(Angle(degrees: getRotationAmount()))
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.spring()) {
+                                offset.width = value.translation.width
+                                offset.height = value.translation.height
+                            }
+                        }
+                        .onEnded { value in
+                            offset = value.translation
+                            withAnimation(.spring()) {
+                                offset = CGSize(width: 0, height: 0)
+                            }
+                        }
+                )
+        }
+    }
+    
+    func getScaleAmount() -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount/max
+        return 1.0 - 0.5*min(percentage,0.5)*0.5
+    }
+    
+    func getRotationAmount() -> Double {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = offset.width
+        let percentage = currentAmount / max
+        let percentageAsDouble = Double(percentage)
+        let maxAngle: Double = 10
+        return percentageAsDouble*maxAngle
+    }
+}
+
+struct DragGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DragGestureBootcamp()
+    }
+}
---
+++ SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
@@ -0,0 +1,87 @@
+//
+//  DragGestureBootcamp2.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Artem on 24.06.2023.
+//
+
+import SwiftUI
+
+struct DragGestureBootcamp2: View {
+    
+    @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85
+    @State var currentDragOffsetY: CGFloat = 0
+    @State var endingOffsetY: CGFloat = 0
+    
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            MySignUpView()
+                .offset(y: startingOffsetY)
+                .offset(y: currentDragOffsetY)
+                .offset(y: endingOffsetY)
+                .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        withAnimation(.spring()){
+                            currentDragOffsetY = value.translation.height
+                        }
+                    }
+                    .onEnded() { value in
+                        withAnimation(.spring()) {
+                        if currentDragOffsetY < -250 {
+                                endingOffsetY = -startingOffsetY
+                            //offsets cancel each other!
+                        } else  if endingOffsetY != 0 && currentDragOffsetY > 250 {
+                             endingOffsetY = 0
+                        }
+                        currentDragOffsetY = 0
+
+                        }
+                    }
+                
+                )
+            Text("\(currentDragOffsetY)")
+        }
+        .ignoresSafeArea(edges:.bottom)
+    }
+}
+
+struct DragGestureBootcamp2_Previews: PreviewProvider {
+    static var previews: some View {
+        DragGestureBootcamp2()
+    }
+}
+
+struct MySignUpView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "chevron.up")
+                .padding(.top)
+            Text("Sign up")
+                .font(.headline)
+                .fontWeight(.semibold)
+            
+            Image(systemName: "flame.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100, alignment: .center)
+            
+            Text("This is the description for our app. This is my SwiftUI DragGesture trainig!")
+                .multilineTextAlignment(.center)
+            
+            Text("CREATE AN ACCOUNT")
+                .foregroundColor(.white)
+                .font(.headline)
+                .padding()
+                .padding(.horizontal)
+                .background(Color.black.cornerRadius(10))
+            Spacer()
+            
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+    }
+}
